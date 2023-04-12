1852c79d4baa19ad88c9721805f2e92e5b5899fe
Carl Corneil <cfc@ssb.no>
2023-04-12 13:54:35 +0200

    endringer6

---
+++ utdanning_fellesfunksjoner/kvalitet/innledende/rapport.py
@@ -0,0 +1,26 @@
+def forenklet():
+    pass
+
+
+def forenklet_versjon():
+    pass
+
+
+def forenklet_versjon_uh():
+    pass
+
+
+def lager():
+    pass
+
+
+def lager_versjon():
+    pass
+
+
+def detaljert():
+    pass
+
+
+def detaljert_type():
+    pass
