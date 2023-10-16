11903813a048649ae938b3988032e833e1d3a210
Corneil, Carl Franklin <Carl.Corneil@ssb.no>
2023-10-16 13:47:31 +0200

    open and save

--- utd_felles/katalog/katalog.py
+++ utd_felles/katalog/katalog.py
@@ -1,16 +1,41 @@
+import dapla as dp
+from pathlib import Path
 import pandas as pd
 from io import StringIO
+from utd_felles.utd_felles_config import UtdFellesConfig
+
+
+
+
+def create_utd_katalog(key_col_name: str, extra_cols: list = None) -> UtdKatalog:
+    # Workaround empty-list-parameter-mutability-issue
+    if extra_cols is None:
+        extra_cols = []
+
+    # Make empty dataset with recommended columns
+    df = pd.DataFrame()
+    df.columns = [key_col_name, "username", "edited_time", "expiry_date", "validity"]
+    
+    # Ask for metadata / Recommend not making katalog
+    metadata = {}
+    metadata["team"] = input("Ansvarlig team for katalogen: ")
+    # Hva mer?
+    
+    return UtdKatalog(df, key_col_name, **metadata)
 
 
 class UtdKatalog:
     def __init__(self,
-                 data: pd.DataFrame,
+                 path: str = "",
                  key_col: str,
                  **metadata,
                 ):
-        self.data = data
+        self.path = path
         self.key_col = key_col
-        for key, value in metadata.items():
+        self.data, self.metadata = self.get_data(self.path)
+        self.metadata = {self.metadata, **metadata}
+        # Make metadata available directly below object
+        for key, value in self.metadata.items():
             setattr(self, key, value)
 
     def __str__(self):
@@ -24,6 +49,47 @@ class UtdKatalog:
         result += buf.getvalue()
         return result
 
+    
+    def get_data(self, path: str = "") -> tuple(pd.DataFrame, dict):
+        """Get the data and metadata for the catalogue, dependant on the environment we are in"""
+        if not path:
+            path = self.path
+        if UtdFellesConfig().MILJO == "PROD":
+            path_kat = Path(path)
+            path_metadata = (path_kat.parent / (str(path_kat.stem) + "__META")).with_suffix(".json")
+            metadata = {}
+            if os.path.isfile(path_metadata):
+                with open(path_metadata, "r") as metafile:
+                    metadata = json.load(metafile)
+            return pd.read_parquet(path_kat), metadata
+        elif UtdFellesConfig().MILJO == "DAPLA":
+            path_metadata = path.replace(".parquet", "_META.json")
+            try:
+                with dp.FileClient.gcs_open(path_metadata, "r") as metafile:
+                    metadata = json.load(metafile)
+            except:
+                metadata = {}
+            return dp.read_pandas(path), metadata
+    
+    def save(self, path: str = "") -> None:
+        """Stores class to disk in prod or dapla as parquet, also stores metadata as json?"""
+        if not path:
+            path = self.path           
+        if UtdFellesConfig().MILJO == "PROD":
+            self.data.to_parquet(path)
+            path_kat = Path(path)
+            if self.metadata:
+                path_metadata = (path_kat.parent / (str(path_kat.stem) + "__META")).with_suffix(".json")
+                with open(path_metadata, "w") as metafile:
+                    metafile.write(self.metadata)
+        elif UtdFellesConfig().MILJO == "DAPLA":
+            dp.write_pandas(self.data, path)
+            if self.metadata:
+                path_metadata = path.replace(".parquet", "_META.json")
+                with dp.FileClient.gcs_open(path_metadata, "w") as metafile:
+                    metafile.write(self.metadata)
+        return None
+    
     def to_dict(self, 
                 col: str = "", 
                 level: int = 0,
