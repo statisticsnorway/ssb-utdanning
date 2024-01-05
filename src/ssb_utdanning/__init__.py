2a38ae7e5e17803f6a53c876e79337a73871f799
Carl F. Corneil <aecorn@gmail.com>
2024-01-05 11:57:15 +0100

    move package code to correct folder

--- src/ssb_utdanning/__init__.py
+++ src/ssb_utdanning/__init__.py
@@ -1,1 +1,12 @@
-"""SSB Utdanning Fellesfunksjoner."""
+from utd_felles.format import info_stored_formats, get_format, UtdFormat
+
+
+from importlib import import_module
+
+
+
+
+
+
+__version__ = "0.0.1"
+__all__ = []
