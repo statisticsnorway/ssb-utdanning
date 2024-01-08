308f08dd4c79c7f7b01375366d0a57fb3433dde2
Carl F. Corneil <aecorn@gmail.com>
2024-01-08 08:43:55 +0100

    test imports returns

--- tests/test_UtdFormat.py
+++ tests/test_UtdFormat.py
@@ -1,22 +1,22 @@
-from ssb_utdanning import UtdFormat
+from ssb_utdanning.format.formats import UtdFormat
 
 
-def test_UtdFormat_simplekey():
+def test_UtdFormat_simplekey() -> None:
     frmt = UtdFormat({"10": "teens"})
     assert frmt["10"] == "teens"
 
 
-def test_UtdFormat_intstr_confusion():
+def test_UtdFormat_intstr_confusion() -> None:
     frmt = UtdFormat({"10": "teens"})
     assert frmt[10] == "teens"
 
 
-def test_UtdFormat_range():
+def test_UtdFormat_range() -> None:
     frmt = UtdFormat({"10-20": "teens"})
     assert frmt[12] == "teens"
 
 
-def test_UtdFormat_add_range():
+def test_UtdFormat_add_range() -> None:
     frmt = UtdFormat()
     frmt["10-20"] = "teens"
     assert frmt[12] == "teens"
