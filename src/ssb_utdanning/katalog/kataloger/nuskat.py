ab3b56876de8142dfa0b1a3cf02f52028b44f744
Carl F. Corneil <aecorn@gmail.com>
2024-03-11 10:48:25 +0100

    fixing specific katalogs

--- src/ssb_utdanning/katalog/kataloger/nuskat.py
+++ src/ssb_utdanning/katalog/kataloger/nuskat.py
@@ -2,18 +2,28 @@ from functools import lru_cache
 
 import dapla as dp
 import pandas as pd
-from utd_felles.data.dtypes import auto_dtype
-from utd_felles.katalog.katalog import UtdKatalog
-from utd_felles.utd_felles_config import UtdFellesConfig
+from fagfunksjoner import auto_dtype
+
+from ssb_utdanning.config import REGION
+from ssb_utdanning.katalog.katalog import UtdKatalog
 
 
 @lru_cache(1)
 def get_nuskat(path: str = "", from_date: str = "") -> UtdKatalog:
-    if UtdFellesConfig().MILJO == "PROD":
+    """Nuskat is a catlogue maintained by 360 that connects more data to each nus2000-code.
+
+    Args:
+        path (str): The path as a string where the nus-katalog is located.
+        from_date (str): If you do not want the latest nuskat, specify a date here.
+
+    Returns:
+        UtdKatalog: A UtdKatalog object with the nuskat-data.
+    """
+    if REGION == "ON_PREM":
         if not path:
             path = "/ssb/stamme01/utd/nuskat/wk16/nus2000/nus2000.sas7bdat"
         nuskat = auto_dtype(pd.read_sas(path))
-    elif UtdFellesConfig().MILJO == "DAPLA":
+    elif REGION == "DAPLA":
         if not path:
             path = ""  # Fix later,use from_date if versioned
         nuskat = dp.read_pandas(path)
--- src/ssb_utdanning/katalog/kataloger/skoleregister.py
+++ src/ssb_utdanning/katalog/kataloger/skoleregister.py
@@ -3,32 +3,47 @@ from functools import lru_cache
 from pathlib import Path
 
 import pandas as pd
-from utd_felles.data.dtypes import auto_dtype
-from utd_felles.katalog.katalog import UtdKatalog
-from utd_felles.utd_felles_config import UtdFellesConfig
+from fagfunksjoner import auto_dtype
+
+from ssb_utdanning.config import REGION
+from ssb_utdanning.katalog.katalog import UtdKatalog
 
 
 @lru_cache(50)
 def get_skoleregister(from_date: str = "") -> UtdKatalog:
+    """Get the skoleregister as an UtdKatalog from a specific date.
+
+    Args:
+        from_date (str): The date to get the skoleregister from. Defaults to "".
+
+    Returns:
+        UtdKatalog: The skoleregister as an UtdKatalog.
+    """
     skolereg_dates = skoleregister_dates()
     if not from_date:
         from_date = sorted(skolereg_dates.keys())[-1]
-
-    if UtdFellesConfig().MILJO == "PROD":
+    if REGION == "ON_PREM":
         df = auto_dtype(pd.read_sas(skolereg_dates[from_date]))
-    elif UtdFellesConfig().MILJO == "DAPLA":
+    elif REGION == "DAPLA":
         return  # Write later
 
-    return UtdKatalog(df, key_col="orgnr", year=year, path=skolereg_dates[from_date])
+    return UtdKatalog(
+        df, key_col="orgnr", from_date=from_date, path=skolereg_dates[from_date]
+    )
 
 
-def skoleregister_dates() -> dict:
-    if UtdFellesConfig().MILJO == "PROD":
+def skoleregister_dates() -> dict[str, str]:
+    """Gets the dates from the skoleregisterfiles, has the date as the key.
+
+    Returns:
+        dict[str, str]: The dates as the key and the path as the value of the skoleregisterfiles.
+    """
+    if REGION == "ON_PREM":
         base_path = "/ssb/stamme01/utd/kat/skolereg/"
         paths = [Path(x) for x in glob.glob(base_path + "*.sas7bdat")]
         date_paths = sorted([x for x in paths if x.stem[1:].isdigit()])
         date_paths_dict = {x.stem[1:5]: x for x in date_paths}
         # print(year_paths_dict.keys())
         return date_paths_dict
-    elif UtdFellesConfig().MILJO == "DAPLA":
+    elif REGION == "DAPLA":
         return  # Write later
