bcedfef0e8aee78dab5b7d485e792a451b7786e1
aecorn <cfc@ssb.no>
2025-05-27 13:02:20 +0000

    bof skole

---
+++ demo/test_bof.py
@@ -0,0 +1,22 @@
+# ---
+# jupyter:
+#   jupytext:
+#     formats: py:percent
+#     text_representation:
+#       extension: .py
+#       format_name: percent
+#       format_version: '1.3'
+#       jupytext_version: 1.17.0
+#   kernelspec:
+#     display_name: ssb-utdanning
+#     language: python
+#     name: ssb-utdanning
+# ---
+
+# %%
+from ssb_utdanning.register.bof import bof_skole
+
+# %%
+bof = bof_skole()
+
+# %%
--- src/ssb_utdanning/register/bof.py
+++ src/ssb_utdanning/register/bof.py
@@ -1,9 +1,13 @@
 import pandas as pd
 from pathlib import Path
 import datetime
+import duckdb
+
+from fagfunksjoner.paths.versions import get_latest_fileversions
+from ssb_utdanning.utdanning_logger import logger
 
 BASE_PATH = Path("/buckets/shared/vof/situttak/")
-SITUTTAK_PATH = BASE_PATH / "vof-situasjonsuttak_data/klargjorte-data/parquet")
+SITUTTAK_PATH = BASE_PATH / "vof-situasjonsuttak_data/klargjorte-data/parquet"
 UTD_NACEKODER = [
     '88.911', '85.100', '85.201', '85.202', '85.203', '85.601',
     '85.310', '85.320', '85.609', '85.510', '85.521', '85.522', '85.529',
@@ -25,15 +29,47 @@ def check_bof_available(raise_error: bool = False) -> bool:
         raise FileNotFoundError("Cant find bof, apply for acces through LDA and mount vof + situttak as shared bucket, so it mounts locally in dapla lab.")
     return False
 
-def bof_skole(reftime: datetime.datetime | None = None) -> pd.DataFrame:
+def bof_skole(reftime: datetime.datetime | None = None,
+              bof_path: str | None = None) -> pd.DataFrame:
     check_bof_available(raise_error=True)
     
-    if reftid is None:
-        reftid = datetime.datetime.now()
-    if not isinstance(reftid, datetime.datetime):
-        raise TypeError("Reftime must be a datetime.")
-    reftid.strftime("%Y-%m")
-    SITUTTAK_PATH.glob()
+    if not bof_path:
+        if reftime is None:
+            bof_path = get_latest_bof_path()
+        elif not isinstance(reftime, datetime.datetime):
+            raise TypeError("Reftime must be a datetime.")
+        else:
+            paths = SITUTTAK_PATH.glob(f"vof-situasjonsuttak_p{reftid.strftime('%Y-%m')}_v1.parquet")
+            if len(paths) == 0:
+                raise ValueError("Found no vof situttak yo")
+            bof_path = Path(get_latest_fileversions([str(paths[0])])[0])
+
+    return load_bof_filtered(bof_path)
+
+def get_latest_bof_path() -> Path:
+    bof_files = sorted(list(SITUTTAK_PATH.glob("*.parquet")))
+    if not bof_files:
+        logger.error("Fant ingen BOF-filer i katalogen.")
+        raise FileNotFoundError("Ingen BOF-filer funnet i katalogen.")
     
-    
+    selected_file = Path(get_latest_fileversions([str(bof_files[-1])])[0])
+    logger.info(f"Valgt BOF-fil: {selected_file}")
+    return selected_file
+
+def load_bof_filtered(bof_path: Path) -> pd.DataFrame:
+    logger.info(f"Leser BOF med DuckDB fra: {bof_path}")
+    quoted_codes = ", ".join([f"'{code}'" for code in UTD_NACEKODER])
+    utdfilter_sql = " OR ".join([f'nace{i}_sn07 IN ({quoted_codes})' for i in range(1, 4)])
+    query = f"""
+        SELECT {', '.join(KOLONNER)}
+        FROM read_parquet('{bof_path}')
+        WHERE {utdfilter_sql}
+    """
+    try:
+        df_bof = duckdb.query(query).to_df()
+        logger.info(f"Antall rader etter filter: {len(df_bof)}")
+        return df_bof
+    except Exception as e:
+        logger.error(f"Feil ved lesing med DuckDB: {e}")
+        return pd.DataFrame()
     
